c3ed3942a98c89bc3ecaa774da78b027ea0cec07
SafoPost <s.i.post@mail.ru>
2021-06-21 13:31:35 +0500

    Added feedback-slider

--- individual/public/js/script.js
+++ individual/public/js/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
     $("#gallerySlider").lightSlider({
-        // autoWidth: true,
-        pager: true,
-        currentPagerPosition: 'middle',
         item: 1,
         useCSS: false,
         loop: true,
@@ -13,5 +10,17 @@ $(document).ready(function() {
         thumbMargin: 12,
         prevHtml: '<button class="button button--gallery button--gallery--prev"></button>',
         nextHtml: '<button class="button button--gallery button--gallery--next"></button>',
+        pager: false
+    });
+
+    $("#feedbackSlider").lightSlider({
+        adaptiveHeight: true,
+        item: 1,
+        useCSS: false,
+        loop: true,
+        slideMargin: 0,
+        prevHtml: '<button class="button button--feedback button--feedback--prev"></button>',
+        nextHtml: '<button class="button button--feedback button--feedback--next"></button>',
+        pager: false
     });
 });
